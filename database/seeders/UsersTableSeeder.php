a265e5b8fd4a8985e888ee02a7eb4cac07818d8d
whbGit <weihuanbin19931016@hotmail.com>
2022-05-08 18:13:11 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,8 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'Summer';
         $user->email = 'summer@example.com';
+        $user->password = bcrypt('123123');
+        $user->is_admin = true;
         $user->save();
     }
 }
